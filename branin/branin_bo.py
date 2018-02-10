4fe3021a69df9b9705b8e3d5cacf4b3a7da4f7b5
Federico <29635356+federicovitti@users.noreply.github.com>
2018-02-10 13:04:53 +0100

    Add files via upload

---
+++ branin/branin_bo.py
@@ -0,0 +1,21 @@
+import numpy as np
+from bayesian_optimization import BayesianOptimization
+from functions import meno_branin
+
+bounds = np.array([[-5., 0.],[10., 15.]])
+opt_value = 0.397887
+n = 20
+cv_iter = 10
+n_iter = 20
+y_bo = np.empty((1, cv_iter, n_iter))
+err_bo = np.empty((1, cv_iter, n_iter))
+times_bo = np.empty((1, cv_iter, n_iter))
+for fold in range(cv_iter):
+	bo = BayesianOptimization(lambda x: meno_branin(x), bounds, opt_value)
+	y_best, tot_err, tot_time = bo.maximize(init_points=n, n_iter=n_iter, kappa=2)
+	y_bo[0, fold] = y_best
+	err_bo[0, fold] = tot_err
+	times_bo[0, fold] = tot_time
+	
+	del bo, y_best, tot_err, tot_time
+
